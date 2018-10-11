0be0803a388be8714929390dd63c64ab46703571
Hugo Tasselli <hugo.tasselli@gmail.com>
2018-10-11 15:50:20 +0200

    Fichier pour le fréro Ajout de produit en tant qu'admin

--- fonctions/removeProd.php
+++ fonctions/removeProd.php
@@ -6,8 +6,9 @@
 		include("../Donnees.inc.php");
 			$mysqli=mysqli_connect($host.":".$port,$user,$pass) or die("Problème de création de la base :".mysqli_error());
 			mysqli_select_db($mysqli,$base) or die("Impossible de sélectionner la base : $base");
-			
-			query($mysqli,'delete from produits where id_prod = '.$_POST["item"]);
+
+			query($mysqli,'delete from produits where id_prod = '.$_POST["item"],'delete from appartient where id_prod = '.$_POST["item"],
+			'delete from appartient2 where id_prod = '.$_POST["item"]);
 			mysqli_close($mysqli);
 	}
 	else{

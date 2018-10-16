a72428a5fe9c666bf075338050eb0dbcb129871f
Unknown <julian.beaudru@protonmail.com>
2018-10-16 23:36:01 +0200

    Fonctionnel mais pas sécure !
    -- 1er problème : Besoin d'informations du compte connectés à récupérer dans les variables $_SESSION. Il m'a fallu rajouter la collecte de données de la BDD pour tout mettre dans $_session lors de la connexion de l'utilisateur. 
    --Deuxième problème : ID_Client attend un int alors qu'on lui donne un string, du coup j'ai changé l'intitulé de la colonne en Client puis le type en Varchar

--- fonctions/Login.php
+++ fonctions/Login.php
@@ -3,17 +3,23 @@ session_start();
 								include("../Parametres.php");
 								include("../Fonctions.inc.php");
 								include("../Donnees.inc.php");
-
 								 $mysqli=mysqli_connect($host.":".$port,$user,$pass) or die("Problème de création de la base :".mysqli_error());
 								 mysqli_select_db($mysqli,$base) or die("Impossible de sélectionner la base : $base");
 										  	$login = trim(mysqli_real_escape_string($mysqli,$_POST["login"]));
 										  	$pass = $_POST["password"];
-										  	$str = "SELECT LOGIN,PASS,EMAIL FROM USERS WHERE LOGIN = '".$login."'";
+										  	$str = "SELECT LOGIN,PASS,EMAIL,NOM,PRENOM,ADRESSE,CODEP,VILLE,TELEPHONE,SEXE FROM USERS WHERE LOGIN = '".$login."'";
 											$result = query($mysqli,$str) or die ("Impossible de se connection à la base de données<br>");
 											if(mysqli_num_rows($result)>0){
 												$row = mysqli_fetch_assoc($result);
 													if(password_verify($pass, $row["PASS"])){
 														$_SESSION["login"] = $row["LOGIN"];
+														$_SESSION["CIVILITE"] = $row["SEXE"];
+														$_SESSION["NOM"] = $row["NOM"];
+														$_SESSION["PRENOM"] = $row["PRENOM"];
+														$_SESSION["ADRESSE"] = $row["ADRESSE"];
+														$_SESSION["CP"] = $row["CODEP"];
+														$_SESSION["VILLE"] = $row["VILLE"];
+														$_SESSION["TELEPHONE"] = $row["TELEPHONE"];
 														unset($return);
 														$return["msg"] = "L'utilisateur est connecté";
 														mysqli_close($mysqli);
